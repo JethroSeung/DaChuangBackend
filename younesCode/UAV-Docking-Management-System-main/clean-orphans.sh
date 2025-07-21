b02400521aeedcbb85a68c8977c987edeb92e24e
JethroSung <149648366+JethroSeung@users.noreply.github.com>
2025-07-21 19:01:58 +0800

    Delete younesCode/UAV-Docking-Management-System-main directory

--- younesCode/UAV-Docking-Management-System-main/clean-orphans.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Stop and remove the orphaned uav-db container
-docker stop uav-db || true
-docker rm uav-db || true
-
-# Launch docker-compose with --remove-orphans flag
-docker-compose up --remove-orphans
